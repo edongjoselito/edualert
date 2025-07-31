5ec2790da1282949c6a6961e7e9014f892b943a0
jhunlio2023 <jhunlio@gmail.com>
2025-07-31 08:32:54 +0800

    dfas

--- application/views/pages/dashboard_school.php
+++ application/views/pages/dashboard_school.php
@@ -19,7 +19,7 @@
                                         <div class="text-white media">
                                             <div class="media-body align-self-center">
                                                 <h2 class="my-0 text-white"><a href="<?= base_url(); ?>Pages/incident_list/0" class="text-white"><span data-plugin="counterup"><?= $incident->num_rows(); ?></span></a></h2>
-                                                <p class="mb-0">Total Incident Report</p>
+                                                <p class="mb-0">New Case</p>
                                             </div>
                                             <i class="ion-ios-pricetag"></i>
                                         </div>
@@ -33,7 +33,63 @@
                                         <div class="text-white media">
                                             <div class="media-body align-self-center">
                                                 <h2 class="my-0 text-white"><a href="<?= base_url(); ?>Pages/incident_list/1" class="text-white"><span data-plugin="counterup"><?= $noaction->num_rows(); ?></a></span></h2>
-                                                <p class="mb-0">Total Incident with action</p>
+                                                <p class="mb-0">Ongoing Case</p>
+                                            </div>
+                                            <i class="ion-ios-pricetag"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-xl-4 col-sm-4">
+                                <div class="card bg-info">
+                                    <div class="card-body widget-style-2">
+                                        <div class="text-white media">
+                                            <div class="media-body align-self-center">
+                                                <h2 class="my-0 text-white"><a href="<?= base_url(); ?>Pages/incident_list/1" class="text-white"><span data-plugin="counterup"><?= $noaction->num_rows(); ?></a></span></h2>
+                                                <p class="mb-0">Resolved</p>
+                                            </div>
+                                            <i class="ion-ios-pricetag"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-xl-4 col-sm-4">
+                                <div class="card bg-info">
+                                    <div class="card-body widget-style-2">
+                                        <div class="text-white media">
+                                            <div class="media-body align-self-center">
+                                                <h2 class="my-0 text-white"><a href="<?= base_url(); ?>Pages/incident_list/1" class="text-white"><span data-plugin="counterup"><?= $noaction->num_rows(); ?></a></span></h2>
+                                                <p class="mb-0">Case Endorsed</p>
+                                            </div>
+                                            <i class="ion-ios-pricetag"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-xl-4 col-sm-4">
+                                <div class="card bg-info">
+                                    <div class="card-body widget-style-2">
+                                        <div class="text-white media">
+                                            <div class="media-body align-self-center">
+                                                <h2 class="my-0 text-white"><a href="<?= base_url(); ?>Pages/incident_list/1" class="text-white"><span data-plugin="counterup"><?= $noaction->num_rows(); ?></a></span></h2>
+                                                <p class="mb-0">Terminated</p>
+                                            </div>
+                                            <i class="ion-ios-pricetag"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-xl-4 col-sm-4">
+                                <div class="card bg-info">
+                                    <div class="card-body widget-style-2">
+                                        <div class="text-white media">
+                                            <div class="media-body align-self-center">
+                                                <h2 class="my-0 text-white"><a href="<?= base_url(); ?>Pages/incident_list/1" class="text-white"><span data-plugin="counterup"><?= $noaction->num_rows(); ?></a></span></h2>
+                                                <p class="mb-0">For Tracing</p>
                                             </div>
                                             <i class="ion-ios-pricetag"></i>
                                         </div>
