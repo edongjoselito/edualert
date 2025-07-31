61cbe7ac75fd32a77c493e087eef275e7fd9338b
edongjoselito <143322244+edongjoselito@users.noreply.github.com>
2025-07-31 19:50:59 +0800

    commit

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -11,7 +11,7 @@
                     <div class="container-fluid">
                         <div class="row">
                             <div class="col-md-12">
-                                2022 - <?= date('Y'); ?> &copy;
+                                &copy; EduAlert v1.0
                             </div>
                         </div>
                     </div>
